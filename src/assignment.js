07d782a631cfbf91a045d4ff876e558a28c6feb8
babawande01 <46625322+babawande01@users.noreply.github.com>
2023-08-24 15:56:44 +0000

    I attempted all three challenges

--- src/assignment.js
+++ src/assignment.js
@@ -16,11 +16,14 @@ const assignment = {};
  */
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i = 1; i<=destination; i++){
+      sum += i;
+    }
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -39,6 +42,16 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for (let i = 1; i <= destination; i++) {
+    if (i % 2 === 0) {
+    count++;
+     sum += i;
+     arrayOfEvenNumbers.push(i);
+    }
+  }
+
+    
+
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +62,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
@@ -68,11 +81,15 @@ function countEvenNumbersWithin(destination) {
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
     let result = [];
+    for (let celsius of arrayOfNumbers) {
+    let fahrenheit = Math.trunc((celsius * 9/5) + 32);
+    result.push(fahrenheit);
+  }
 
     return result;
 }
 
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 // ========================
 // DO NOT EDIT THIS BLOCK
